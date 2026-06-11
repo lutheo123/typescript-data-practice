c14c5a820de98d444d0f0275b037d60b8d84d67e
lutheo123 <lucasyouti@gmail.com>
2026-06-11 11:10:08 -0300

    feat(main script): add handleData function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,4 @@
-"use strict";
+import { fetchData } from "./modules/fetchData";
+async function handleData() {
+    const data = fetchData("https://api.origamid.dev/json/transacoes.json");
+}
---
+++ src/script.ts
@@ -0,0 +1,5 @@
+import { fetchData } from "./modules/fetchData";
+
+async function handleData() {
+    const data = fetchData("https://api.origamid.dev/json/transacoes.json");
+}
