c604bf750bdf4c4f0ee920785e8e5956567e8426
lutheo123 <lucasyouti@gmail.com>
2026-06-11 16:32:44 -0300

    feat(countStatus): create countStatus function

--- src/modules/countStatus.ts
+++ src/modules/countStatus.ts
@@ -1,2 +1,16 @@
 import { Transaction } from "./fetchData";
 
+export function countStatus(data: Transaction[]): {
+    paid: number,
+    refused: number,
+    waiting: number,
+    reversed: number,
+} {
+    let paid = 0, refused = 0, waiting = 0, reversed = 0;
+    return {
+        paid: paid,
+        refused: refused,
+        waiting: waiting,
+        reversed: reversed,
+    }
+}
