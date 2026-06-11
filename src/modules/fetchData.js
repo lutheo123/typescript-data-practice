e7e7db3265c86b3a65a0344b18d0460f498d3630
lutheo123 <lucasyouti@gmail.com>
2026-06-11 11:06:45 -0300

    feat(fetchData): handle fetch error

---
+++ src/modules/fetchData.js
@@ -0,0 +1,10 @@
+export async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok)
+            throw new Error("Error" + response.status);
+        const dataJSON = await response.json();
+        return dataJSON;
+    }
+    catch (e) { }
+}
--- src/modules/fetchData.ts
+++ src/modules/fetchData.ts
@@ -1,6 +1,7 @@
 export async function fetchData(url: string) {
     try {
         const response = await fetch(url);
+        if (!response.ok) throw new Error("Error" + response.status);
         const dataJSON = await response.json();
         return dataJSON;
     } catch (e) {}
