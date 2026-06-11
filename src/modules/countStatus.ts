c8a170d388ab2948dbcc11c264075011f7530563
lutheo123 <lucasyouti@gmail.com>
2026-06-11 16:35:14 -0300

    feat(countStatus): add countStatus logic

--- src/modules/countStatus.ts
+++ src/modules/countStatus.ts
@@ -7,6 +7,14 @@ export function countStatus(data: Transaction[]): {
     reversed: number,
 } {
     let paid = 0, refused = 0, waiting = 0, reversed = 0;
+    data.forEach(item => {
+        const dataStatus = item.Status;
+        if (dataStatus === "Paga") paid++;
+        else if (dataStatus === "Recusada pela operadora de cartão") refused++;
+        else if (dataStatus === "Aguardando Pagamento") waiting++;
+        else if (dataStatus === "Estornada") reversed++;
+    })
+
     return {
         paid: paid,
         refused: refused,
