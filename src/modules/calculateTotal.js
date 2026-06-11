ec32ce8bdacc8fa98f62208b3494744dab8e0907
lutheo123 <lucasyouti@gmail.com>
2026-06-11 13:46:52 -0300

    feat(calculateTotal): implement calculateTotal function

--- src/modules/calculateTotal.js
+++ src/modules/calculateTotal.js
@@ -1,9 +1,14 @@
-function mapValores(data) {
+function mapValues(data) {
     const validValues = data.filter(item => item["Valor (R$)"] !== '-');
     console.log(validValues);
     const values = validValues.map(item => {
         const valueAjusted = item["Valor (R$)"].replaceAll(".", "").replace(",", ".");
         return Number(valueAjusted);
     });
+    return values;
 }
-export {};
+export function calculateTotal(data) {
+    const values = mapValues(data);
+    const totalValue = values.reduce((acc, cur) => acc + cur, 0);
+    console.log(totalValue);
+}
--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 import { fetchData } from "./modules/fetchData.js";
+import { calculateTotal } from "./modules/calculateTotal.js";
 async function handleData() {
     const data = await fetchData("https://api.origamid.dev/json/transacoes.json");
+    if (data) {
+        calculateTotal(data);
+    }
 }
 handleData();
